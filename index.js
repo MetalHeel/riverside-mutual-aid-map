8d111169ef7cfae658270018c8a2a65d92417c32
MetalHeel <MetalHeel@aol.com>
2020-04-08 18:44:06 -0500

    Let Heroku set the port.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 6969;
 
 app.get('/', function (request, response) {
 	response.send('Hello World!');
 });
 
-app.listen(6969);
+app.listen(port);
