d41cf49d475b8ccf56f3f0178d229c49020a027f
MetalHeel <MetalHeel@aol.com>
2020-04-22 17:42:12 -0500

    Log volunteer data to see if we're still getting it.

--- public/js/mapSetup.js
+++ public/js/mapSetup.js
@@ -12,6 +12,7 @@ function initMap() {
 			return;
 		}
 		var volunteerData = JSON.parse(xmlHttp.response);
+		console.log(volunteerData);
 		setupMap(volunteerData.feed.entry);
 	}
 	xmlHttp.open("GET", "https://spreadsheets.google.com/feeds/cells/1V4F02LbP8feQXTKX_l1ES9kv4udM09l59KPWr4r4J9w/1/public/full?alt=json", true);
